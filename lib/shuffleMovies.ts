59f646a17fbbaf91ef87a6674937b874a1e3fd00
Gokul <gklgrme@gmail.com>
2026-02-13 10:26:46 +0530

    fea:added fisher shuffle

---
+++ lib/shuffleMovies.ts
@@ -0,0 +1,9 @@
+
+//fisher yates shuffle
+function shuffleMovies<T>(movies: T[]) {
+    for (let i = movies.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [movies[i], movies[j]] = [movies[j], movies[i]];
+    }
+    return movies;
+}
